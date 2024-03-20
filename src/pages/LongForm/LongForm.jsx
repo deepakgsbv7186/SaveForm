c354811750947e31f4ac0e2fdc6ba0a216be210f
deepak.verma <deekgsbv@gmail.com>
2024-03-20 22:38:00 +0530

    modify gender input field from buttons to radio, reduce the handleChange function

--- src/pages/LongForm/LongForm.jsx
+++ src/pages/LongForm/LongForm.jsx
@@ -11,11 +11,10 @@ export default function LongForm() {
     dateOfBirth: "",
   });
 
-  const handleProfilePic = (e) => {
-    // Trigger file input when profile icon is clicked
+  const handleProfilePic = () => {
     const fileInput = document.createElement("input");
     fileInput.type = "file";
-    fileInput.accept = "image/*"; // Accept only image files
+    fileInput.accept = "image/*";
     fileInput.onchange = handleFileChange;
     fileInput.click();
   };
@@ -25,47 +24,45 @@ export default function LongForm() {
     if (file) {
       const reader = new FileReader();
       reader.onload = () => {
-        setUserInput({ ...userInput, profilePic: reader?.result });
+        setUserInput({ ...userInput, profilePic: reader.result });
       };
       reader.readAsDataURL(file);
     }
   };
 
-  const handleTextChange = (e) => {
+  const handleInputChange = (e) => {
     setUserInput({ ...userInput, [e.target.name]: e.target.value });
   };
-  const handleGenderChange = (e, gender) => {
-    e.preventDefault();
-    setUserInput({ ...userInput, gender: gender });
-  };
-  const handleMaritalStatusChange = (e) => {
-    setUserInput({ ...userInput, maritalStatus: e.target.value });
-  };
-  const fullNamePreview = () => {
-    if (!userInput?.firstName && !userInput?.lastName) {
-      return "your full name appears here";
-    }
-    return `${userInput?.firstName} ${userInput?.lastName}`;
-  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log(userInput);
+    // Handle form submission logic here
   };
+
+  const fullNamePreview = () => {
+    const { firstName, lastName } = userInput;
+    if (!firstName && !lastName) {
+      return "Your full name appears here";
+    }
+    return `${firstName} ${lastName}`;
+  };
+
   return (
     <>
       <div className="w-full h-auto bg-[#242424] flex justify-center items-center py-10">
         <div className="w-[90%] lg:w-[50%] h-full rounded-md shadow-md bg-sky-400">
-          <form action="">
+          <form onSubmit={handleSubmit}>
             <h2 className="font-semibold text-white text-center my-4 text-3xl">
               Long Way Register
             </h2>
-            {userInput?.profilePic ? (
+            {userInput.profilePic ? (
               <div
                 onClick={handleProfilePic}
                 className="w-[150px] h-[150px] rounded-full bg-white mx-auto my-10 flex justify-center items-center cursor-pointer"
               >
                 <img
-                  src={userInput?.profilePic}
+                  src={userInput.profilePic}
                   alt="profilepic"
                   className="w-[100%] h-[100%] rounded-full object-cover"
                 />
@@ -80,7 +77,6 @@ export default function LongForm() {
               <h3 className="font-medium text-white text-xl bg-slate-500">
                 General Information
               </h3>
-              {/* first name, last name, full name (preview), gender, date of birth, marital status */}
               <div>
                 <label
                   htmlFor="firstName"
@@ -92,9 +88,9 @@ export default function LongForm() {
                   type="text"
                   name="firstName"
                   id="firstName"
-                  placeholder="enter your first name"
-                  value={userInput?.firstName}
-                  onChange={handleTextChange}
+                  placeholder="Enter your first name"
+                  value={userInput.firstName}
+                  onChange={handleInputChange}
                   className="font-bold text-white text-xl leading-relaxed placeholder:text-slate-300 placeholder:font-normal bg-transparent rounded-md outline-none w-full px-2 py-1 border-2 focus:border-yellow-400"
                 />
               </div>
@@ -107,25 +103,19 @@ export default function LongForm() {
                 </label>
                 <input
                   type="text"
-                  id="lastName"
                   name="lastName"
-                  placeholder="enter your last name"
-                  value={userInput?.lastName}
-                  onChange={handleTextChange}
+                  id="lastName"
+                  placeholder="Enter your last name"
+                  value={userInput.lastName}
+                  onChange={handleInputChange}
                   className="font-bold text-white text-xl leading-relaxed placeholder:text-slate-300 placeholder:font-normal bg-transparent rounded-md outline-none w-full px-2 py-1 border-2 focus:border-yellow-400"
                 />
               </div>
               <div>
-                <label
-                  //   htmlFor="fullName"
-                  className="block text-white text-sm mt-4 mb-1"
-                >
+                <label className="block text-white text-sm mt-4 mb-1">
                   Full Name (Preview)
                 </label>
-                <p
-                  //   id="fullName"
-                  className="opacity-70 font-bold text-white text-xl leading-relaxed border-2 border-white rounded-md px-2 py-1"
-                >
+                <p className="opacity-70 font-bold text-white text-xl leading-relaxed border-2 border-white rounded-md px-2 py-1">
                   {fullNamePreview()}
                 </p>
               </div>
@@ -136,41 +126,43 @@ export default function LongForm() {
                 >
                   Gender
                 </label>
-                <button
-                  id="gender"
-                  name="gender"
-                  onClick={(e) => handleGenderChange(e, "female")}
-                  className={`${
-                    userInput?.gender === "female" && "bg-pink-400"
-                  } text-lg font-semibold text-white border-2 border-pink-400 rounded-md shadow-md px-1 py-1 mr-4`}
-                >
+                <label className="font-bold text-white text-xl mr-6">
+                  <input
+                    type="radio"
+                    name="gender"
+                    value="female"
+                    checked={userInput.gender === "female"}
+                    onChange={handleInputChange}
+                    className="mr-1"
+                  />
                   Female
-                </button>
-                <button
-                  id="gender"
-                  name="gender"
-                  onClick={(e) => handleGenderChange(e, "male")}
-                  className={`${
-                    userInput?.gender === "male" && "bg-blue-600"
-                  } text-lg font-semibold text-white border-2 border-blue-600 rounded-md shadow-md px-1 py-1 mr-4`}
-                >
+                </label>
+                <label className="font-bold text-white text-xl mr-6">
+                  <input
+                    type="radio"
+                    name="gender"
+                    value="male"
+                    checked={userInput.gender === "male"}
+                    onChange={handleInputChange}
+                    className="mr-1"
+                  />
                   Male
-                </button>
+                </label>
               </div>
               <div>
                 <label
                   htmlFor="dateOfBirth"
                   className="block text-white text-sm mt-4 mb-1"
                 >
-                  Date Of Brith
+                  Date Of Birth
                 </label>
                 <input
                   type="date"
                   name="dateOfBirth"
                   id="dateOfBirth"
                   placeholder="DD/MMM/YYYY"
-                  value={userInput?.dateOfBirth}
-                  onChange={handleTextChange}
+                  value={userInput.dateOfBirth}
+                  onChange={handleInputChange}
                   className="font-bold text-white text-xl leading-relaxed placeholder:text-slate-300 placeholder:font-normal bg-transparent rounded-md outline-none w-full px-2 py-1 border-2 focus:border-yellow-400"
                 />
               </div>
@@ -184,21 +176,13 @@ export default function LongForm() {
                 <select
                   name="maritalStatus"
                   id="maritalStatus"
-                  value={userInput?.maritalStatus}
-                  onChange={handleMaritalStatusChange}
+                  value={userInput.maritalStatus}
+                  onChange={handleInputChange}
                   className="font-bold text-white text-xl leading-relaxed placeholder:text-slate-300 placeholder:font-normal bg-transparent rounded-md outline-none w-full px-2 py-2 border-2 focus:border-yellow-400"
                 >
-                  <option className="bg-slate-400" label="Select" value={""} />
-                  <option
-                    className="bg-slate-400"
-                    label="Un-married"
-                    value={"unmarried"}
-                  />
-                  <option
-                    className="bg-slate-400"
-                    label="Married"
-                    value={"married"}
-                  />
+                  <option value="">Select</option>
+                  <option value="unmarried">Un-married</option>
+                  <option value="married">Married</option>
                 </select>
               </div>
             </section>
@@ -227,7 +211,6 @@ export default function LongForm() {
               </button>
               <button
                 type="submit"
-                onClick={handleSubmit}
                 className="hover:bg-green-500 transition-colors duration-200 ease-linear bg-blue-600 flex flex-1 justify-center items-center rounded-md shadow-md p-2 text-white text-lg font-medium"
               >
                 Submit
