d10c9be79d5bd418cb084e593749ab5bde5db6b3
deepak.verma <deekgsbv@gmail.com>
2024-03-22 23:46:05 +0530

    add validation to login

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -1,42 +1,64 @@
 import { Link } from "react-router-dom";
 import Input from "../../components/Input";
-import { useState } from "react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+
+const validationSchema = Yup.object().shape({
+  email: Yup.string().email("Invalid email").required("Email is required."),
+  password: Yup.string()
+    .trim()
+    .min(8, "Al least 8 characters.")
+    .matches(
+      /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/,
+      "Please enter valid password."
+    )
+    .required("Password is required."),
+});
 
 export default function Login() {
-  const [userData, setUserData] = useState({
-    email: "",
-    password: "",
-  });
+  const { values, errors, touched, handleBlur, handleChange, handleSubmit } =
+    useFormik({
+      initialValues: {
+        email: "",
+        password: "",
+      },
+      validationSchema: validationSchema,
+      onSubmit: (values) => handleFormSubmit(values),
+    });
 
-  const handleChange = (e) => {
-    setUserData({ ...userData, [e.target.name]: e.target.value });
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(userData);
+  const handleFormSubmit = (values) => {
+    console.log("handdle", values);
   };
   return (
     <div className="flex justify-center items-center h-screen">
       <div className="p-4 bg-slate-700 rounded-md shadow-md lg:w-3/12 md:w-6/12 w-[90%]">
-        <form onSubmit={handleSubmit} method="post">
+        <form onSubmit={handleSubmit}>
           <Input
             label={"email"}
             placeholder={"abc@email.com"}
             type={"email"}
-            value={userData.email}
+            value={values.email}
             onChange={handleChange}
+            onBlur={handleBlur}
+            errorMessage={errors.email && touched.email ? errors.email : null}
           />
           <Input
             label={"password"}
             placeholder={"your password here..."}
             type={"password"}
             password
-            value={userData.password}
+            value={values.password}
             onChange={handleChange}
+            onBlur={handleBlur}
+            errorMessage={
+              errors.password && touched.password ? errors.password : null
+            }
           />
           <div className="my-6">
-            <button className="text-xl text-white font-semibold text-center bg-slate-500 p-1 w-full rounded-md">
+            <button
+              type="submit"
+              className="text-xl text-white font-semibold text-center bg-slate-500 p-1 w-full rounded-md"
+            >
               Login
             </button>
           </div>
