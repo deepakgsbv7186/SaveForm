d71b078f8f42fe01bbe588755efc508fc0928c56
deepak.verma <deekgsbv@gmail.com>
2024-03-24 12:21:59 +0530

    practiceone start

--- api/routes/router.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { userRouter } from "./user.routes.js";
-
-const router = express.Router();
-
-router.use("/api/auth", userRouter);
-
-export { router };
--- api/routes/user.routes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { UserLogin, UserRegister } from "../controllers/user.controller.js";
-
-const userRouter = express.Router();
-
-userRouter.post("/register", UserRegister);
-userRouter.post("/login", UserLogin);
-
-export { userRouter };
